79f8fd4e1a08ebbb6c6fd23b7524ffe4b76102de
oialequis <kensou201211@gmail.com>
2025-03-04 11:44:53 +0000

    Brench nova

--- main.py
+++ main.py
@@ -3,6 +3,15 @@ import pandas as pd
 import sqlite3 as db
 
 
+
+google_drive_id = "1FtkhmnlAUWmGaFxXDn374UsWZ0k8IYpB"
+
+# Criar um link direto para exibir a imagem
+image_url = f"https://drive.google.com/uc?export=view&id={google_drive_id}"
+
+
+
+
 # Criar conexão com o banco de dados SQLite
 conn = db.connect("database.db", check_same_thread=False)
 cursor = conn.cursor()
@@ -12,43 +21,14 @@ def validar_login(nome, senha):
 
 
 
-def botao_teste():
-    df = pd.read_excel(uploaded_file)
-    df = df.drop(0)
-    df.columns = df.loc[1]
-    df = df[1:]
-
-    # Seleção de colunas com .loc
-    df_filtro = df.loc[:, ['Matricula', 'Nome', 'Data Admis.', 'Sit. Folha', 'Desc.Funcao', 'Desc. Depto', 'CPF']]
-
-    # Conversão e formatação de datas com .loc
-    df_filtro['Matricula'] = df_filtro['Matricula'].astype(str)
-    df_filtro['Matricula'] = df_filtro['Matricula'].str.replace(',', '').str.replace('.', '').astype(str)
-    df_filtro['Data Admis.'] = pd.to_datetime(df_filtro['Data Admis.'], errors='coerce')
-    df_filtro['Data Admis.'] = df_filtro['Data Admis.'].apply(lambda x: x.strftime('%d/%m/%Y') if pd.notna(x) else '')
-
-
-    # Limpeza e substituição de dados com .loc
-    df_filtro.loc[:, 'Sit. Folha'] = df_filtro.loc[:, 'Sit. Folha'].fillna('ATIVO')
-    df_filtro.loc[:, 'Sit. Folha'] = df_filtro.loc[:, 'Sit. Folha'].replace('F', "FÉRIAS")
-    df_filtro.loc[:, 'Sit. Folha'] = df_filtro.loc[:, 'Sit. Folha'].replace('A', "AFASTADO")
-
-    # Remoção de dados com .loc
-    funcao_para_remover = ['AUTONOMO','MEDICO RESIDENTE','ENFERMEIRO RESIDENTE']
-    df_filtro = df_filtro.loc[~df_filtro['Desc.Funcao'].str.lower().isin([funcao.lower() for funcao in funcao_para_remover])]
-
-    grafico_filtrado = df_filtro.loc[:, 'Sit. Folha'].value_counts()
-    # Tratar valores ausentes e converter para inteiro
-    grafico_filtrado = pd.to_numeric(grafico_filtrado, errors='coerce').fillna(0).astype(int)
-
-    # Redefinir os índices da Series
-    grafico_filtrado = grafico_filtrado.reset_index()
-
-    # Renomear as colunas para que o Streamlit as interprete corretamente
-    grafico_filtrado.columns = ['Rótulos', 'Valores']
-
-    st.bar_chart(grafico_filtrado, x='Rótulos', y='Valores')
-    st.write(grafico_filtrado)
+def botao_enviar():
+    if uploaded_file is not None:
+        cursor.execute("INSERT INTO files (name, data) VALUES (?, ?)", 
+                    (uploaded_file.name, uploaded_file.getvalue()))
+        conn.commit()
+        st.success("Arquivo salvo no banco de dados!")
+    
+    
 
 
    
@@ -57,7 +37,6 @@ def login():
         st.success("LOGADO COM SUCESSO")
         st.session_state.show_login = False
         st.rerun()
-    
         
     else:
         st.error("USUÁRIO OU SENHA INCORRETOS")
@@ -85,12 +64,18 @@ if st.session_state.show_login:
             login()
         
 else:
+   if st.session_state.username == "admin":
+      st.title("PAINEL DE ADMINISTRADOR")
      #TELA PRINCIPAL
-     user_conectado = st.session_state.username
-     st.title(f"Bem-vindo {user_conectado}")
+      user_conectado = st.session_state.username
+      st.title(f"Bem-vindo {user_conectado}")
 
-     uploaded_file = st.file_uploader("Escolha uma SRA.xlsx", type=["xlsx"])
-     if st.button("Enviar arquivo."):
-        botao_teste()
-    
+      uploaded_file = st.file_uploader("ESCOLHA UMA IMAGEM.JPG", type=["jpg"])
+      if st.button("Enviar arquivo."):
+        botao_enviar()
+   else:
+       st.title("PAINEL DE USUÁRIO")
+       # Exibir a imagem no app
+       st.image(image_url, caption="Imagem carregada do Google Drive")
+      
      
